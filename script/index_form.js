fee452c2f3915f00b273e8237344707719c386a3
OlegPazny <petr.jakimovets@gmail.com>
2023-05-20 15:54:02 +0300

    полностью рабочая отправка формы поддержки

---
+++ script/index_form.js
@@ -0,0 +1,8 @@
+let form=document.forms['form'];
+
+console.log(form);
+
+// function send_form(){
+//     let name=document.form[0].elements['name'];
+//     console.log(name);
+// }
--- script/support.js
+++ script/support.js
@@ -1,24 +1,41 @@
 let block=document.querySelectorAll('.select-btn');
 let way_label=document.querySelector('.way');
 
+let user_name=document.querySelector('.name');
+let way=document.querySelector('.way_field');
+let topic=document.querySelector('.topic');
+let comment=document.querySelector('.comment');
+
 let flag="byPhone";
 block[0].style.boxShadow="0 0 15px 10px #0000001c";
-way_label.innerHTML="Телефон *"
+way_label.innerHTML="Телефон *";
+let way_name="Телефон";
 
 function change(){
     let radio=document.querySelectorAll('.radio');
+
+    way.value="";
 
     for(const q of radio){
         if(q.checked&&q.value=="byPhone"){
             block[1].style.boxShadow="0 0 7px 2px #0000001c";
             block[0].style.boxShadow="0 0 15px 10px #0000001c";
             flag=q.value;
-            way_label.innerHTML="Телефон *"
+            way_label.innerHTML="Телефон *";
+            way_name="Телефон";
         }else if(q.checked&&q.value=="byMail"){
             block[0].style.boxShadow="0 0 7px 2px #0000001c";
             block[1].style.boxShadow="0 0 15px 10px #0000001c";
             flag=q.value;
-            way_label.innerHTML="Почта *"
+            way_label.innerHTML="Почта *";
+            way_name="Почта";
         }
     }
+}
+
+function sendInfo(){
+    console.log("Имя: "+user_name.value);
+    console.log(way_name+": "+way.value);
+    console.log("Тема: "+topic.value);
+    console.log("Сообщение: "+comment.value);
 }
